8a166b9c574e450b6b6a5ab7b496f714f19a6c2b
jomarjm <71677200+jomarjm@users.noreply.github.com>
2023-04-08 15:57:58 -0400

    fixed fade in. complete.

--- js/main.js
+++ js/main.js
@@ -11,21 +11,46 @@ let typewriter = () => {
         setTimeout(typewriter, speed);
         console.log(i);
     } else {
-        fadeInPicture();
+        setTimeout(fadeInSeagull, 1600);
     }
 };
 
 // Delays typewriter function on load
 let typewriterDelay = () => {
-    setTimeout(typewriter, 1000);
+    setTimeout(typewriter, 2000);
 };
 
 // Executes typewriter functions on load
 window.addEventListener('load', typewriterDelay);
 
+let fadeInSeagull = () => {
+    document.getElementById("seagull").style.opacity = 1;
+}
+
+/*
+
+// For fade in picture
+let opacity = 0;
+
+
+
 // Start picture fade in
 let fadeInPicture = () => {
-    document.getElementById("seagull").style.display = "block";
-    document.getElementById("seagull").style.opacity = 1;
+    setInterval(show, 1);
 };
 
+
+
+let show = () => {
+    let seagull = document.getElementById("seagull");
+    opacity = Number(window.getComputedStyle(seagull).getPropertyValue("opacity"));
+
+    if (opacity < 1) {
+        opacity += 0.5;
+        seagull.style.opacity = opacity
+    } else {
+        clearInterval(0);
+    }
+}
+
+*/
